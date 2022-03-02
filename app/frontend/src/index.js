6e6a4ceddda2090e2c200478ece851354d123ab7
Tom Ragle <tragle@gmail.com>
2022-03-02 09:35:34 -0500

    Add UI

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,5 +1,9 @@
 import * as msal from '@azure/msal-browser';
 
+let idClaims;
+let accessToken;
+let count;
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const SERVER_URI = process.env.SERVER_URI;
@@ -16,25 +20,65 @@ const msalConfig = {
   }
 };
 
-console.log(JSON.stringify(msalConfig));
-
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 const request = {
   scopes: [SCOPE],
 };
 
-console.log(JSON.stringify(request));
-
-msalInstance.handleRedirectPromise().then(async (token) => {
-  if (!token) {
+msalInstance.handleRedirectPromise().then(async (tokenResult) => {
+  if (!tokenResult) {
     console.log('acquiring token via redirect');
     msalInstance.acquireTokenRedirect(request);
   }
-  if (token) {
-    console.log('got token', token);
-    const resp = await fetch(SERVER_URI, { method: 'POST', body: token.accessToken });
-    const body = await resp.json();
-    console.log('got api response', body);
+  if (tokenResult) {
+    console.log('got token(s)', tokenResult);
+    accessToken = tokenResult.accessToken;
+    idClaims = tokenResult.idTokenClaims;
   }
 });
+
+function updateElement(id, text) {
+  const el = document.getElementById(id);
+  el.innerHTML = text; 
+}
+
+function hideElement(id) { 
+  const el = document.getElementByid(id);
+  el.style.display = 'none';
+}
+
+function showElement(id) { 
+  const el = document.getElementByid(id);
+  el.style.display = 'none';
+}
+
+async function callApi(token) {
+  const resp = await fetch(SERVER_URI, { method: 'POST', body: token });
+  const json = await resp.json();
+  console.log('got api response', json);
+  return json;
+}
+
+function syncUI() {
+  if (!idClaims) {
+    hideElement('counter');
+    updateElement('username', '');
+    updateElement('count', '');
+  } else {
+    updateElement('username', idClaims.name);
+    updateElement('count', count);
+    showElement('counter');
+  }
+}
+
+async function refresh() {
+  const countResult = await callApi(accessToken);
+  if (countResult && countResult.count) {
+    count = countResult.count;
+  }
+  syncUI();
+}
+
+
+document.getElementById('refresh-btn').addEventListener("click", refresh); 
